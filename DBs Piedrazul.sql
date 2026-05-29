e1bddbe1003d90f308e77468822af0e09c507e18
JuanPino123 <168316396+JuanPino123@users.noreply.github.com>
2026-05-28 23:26:11 -0500

    Script SQL creación DBs
    Script para crear las bases de datos usadas por el programa Piedra-azul y sus microservicios

---
+++ DBs Piedrazul.sql
@@ -0,0 +1,4 @@
+--Solo ejecuten esto, los nombres son los correctos para el programa, los spring crearán las bases por si solos
+create database usuarios_db;
+create database personas_db;
+CREATE DATABASE citas_db;
